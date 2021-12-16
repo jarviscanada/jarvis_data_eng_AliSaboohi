5fe37533c91abb5130e9c55ed2174d7fe1fa9524
Ali Saboohi <ali.saboohi76@gmail.com>
2021-12-16 19:00:36 +0000

    finished queries.sql

---
+++ linux_sql/sql/queries.sql
@@ -0,0 +1,25 @@
+select cpu_number,
+       id,
+       total_mem
+from host_info
+group by cpu_number,id,total_mem
+order by total_mem desc;
+
+
+select id as "host_id",hostname,aa.ts,aa.avg
+from
+(select  round5(host_usage."timestamp") as ts,CAST((((CAST(RTRIM (total_mem, 'K') as decimal) - memory_free)/CAST(RTRIM (total_mem, 'K') as decimal))*100) as INT) as "avg",host_id as x
+from host_usage
+join host_info
+on host_info.id = host_usage.host_id
+group by ts,avg,host_id
+order by ts
+) aa
+inner join  host_info on id = aa.x
+
+
+select host_id ,round5("timestamp"), count(round5("timestamp"))
+from host_usage
+group by round5("timestamp"), host_id
+having count(round5("timestamp")) < 3
+order by round5(round5("timestamp"));
